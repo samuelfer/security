b83c8985fe91736cdfcfdf96fcc456a489044ca0
Samuel <samuelfesant@gmail.com>
2023-12-09 19:40:59 -0300

    Configure endpoint test

---
+++ src/main/java/com/samuelfer/security/demo/DemoController.java
@@ -0,0 +1,16 @@
+package com.samuelfer.security.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo-controller")
+public class DemoController {
+
+    @GetMapping
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello, Welcome from secured endpoint!");
+    }
+}
