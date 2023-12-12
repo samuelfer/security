2941313e2e9e4d4836d1ce874d0228253f8eb45a
Samuel <samuelfesant@gmail.com>
2023-12-11 21:45:49 -0300

    Add security of methods

--- src/main/java/com/samuelfer/security/demo/DemoController.java
+++ src/main/java/com/samuelfer/security/demo/DemoController.java
@@ -1,6 +1,7 @@
 package com.samuelfer.security.demo;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/demo-controller")
 public class DemoController {
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok("Hello, Welcome from secured endpoint!");
